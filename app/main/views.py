27f34fe098c7a8ef688c0fd48630d86d5c7129cd
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-06-17 14:00:34 +0300

    Add genres template.

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,10 @@ def index():
     title='Home | MOTD'
 
     return render_template('home.html', popular=popular_movies, title=title)
+
+@main.route('/genres')
+def genres():
+
+    title= 'Genres| MOTD'
+
+    return render_template('genres.html', title=title)
